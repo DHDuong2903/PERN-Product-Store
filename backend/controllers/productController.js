ea2723ac49aea68f64ece6d168d5963952a47bce
DHDuong2903 <dohuyduong2903@gmail.com>
2025-06-30 21:29:31 +0700

    add_CRUD_product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,87 @@
-export const getAllProducts = async (req, res) => {
-  
-}
+import { sql } from "../config/db.js";
 
-export const createProduct = async (req, res) => {}
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await sql`
+      SELECT * FROM products
+      ORDER BY created_at DESC
+    `;
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await sql`
+      SELECT * FROM products WHERE id = ${id}
+    `;
+
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const { name, image, price } = req.body;
+
+  if (!name || !image || !price) {
+    return res.status(400).json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const newProduct = await sql`
+      INSERT INTO products (name, image, price)
+      VALUES (${name}, ${image}, ${price})
+      RETURNING *
+    `;
+    res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, image, price } = req.body;
+
+  try {
+    const updatedProduct = await sql`
+      UPDATE products
+      SET name = ${name}, image = ${image}, price = ${price}
+      WHERE id = ${id}
+      RETURNING *
+    `;
+
+    if (updatedProduct.length === 0) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: updatedProduct[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+      DELETE FROM products
+      WHERE id = ${id}
+      RETURNING *
+    `;
+
+    if (deletedProduct.length === 0) {
+      return res.status(404).json({ success: false, message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, data: deletedProduct[0] });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
